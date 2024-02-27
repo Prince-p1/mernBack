16873a3f55e82754fba754cf4766063c32dec88a
Prince Kumar <princekumar15032003@gmail.com>
2024-02-27 15:48:44 +0530

    forth

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ mongoDB();
 
 app.use((req,res,next)=>{
   res.setHeader("Access-Control-Allow-Origin", "https://mernfront-3.onrender.com");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
