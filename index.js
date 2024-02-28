60201efb960bc104680eaaf0f0cede4a22d9bf0e
Prince Kumar <princekumar15032003@gmail.com>
2024-02-28 20:05:41 +0530

    xx

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const mongoDB = require("./db");
 mongoDB();
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://65df3f590c7658161c232a92--quiet-youtiao-2455ac.netlify.app");
+  res.setHeader("Access-Control-Allow-Origin", "https://food-delivery-app-prince.netlify.app/");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "*"); // Use wildcard to allow any headers
   next();
