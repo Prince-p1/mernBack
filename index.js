983fe8d145918bae4945321845c1e35f30f785f9
Prince Kumar <princekumar15032003@gmail.com>
2024-02-28 19:52:38 +0530

    x

--- index.js
+++ index.js
@@ -1,30 +1,56 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const mongoDB = require("./db")
+// const express = require('express')
+// const app = express()
+// const port = 5000
+// const mongoDB = require("./db")
+
+// mongoDB();
+
+// app.use((req,res,next)=>{
+//   res.setHeader("Access-Control-Allow-Origin", "https://65df3f590c7658161c232a92--quiet-youtiao-2455ac.netlify.app");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// })
+
+// app.use(express.json());
+// app.use('/api', require("./Routes/CreatUser"));
+// app.use('/api', require("./Routes/DisplayData"));
+// app.use('/api', require("./Routes/OrderData"));
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+const express = require('express');
+const app = express();
+const port = 5000;
+const mongoDB = require("./db");
 
 mongoDB();
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "https://65df3f590c7658161c232a92--quiet-youtiao-2455ac.netlify.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "*"); // Use wildcard to allow any headers
   next();
-})
+});
 
 app.use(express.json());
 app.use('/api', require("./Routes/CreatUser"));
 app.use('/api', require("./Routes/DisplayData"));
 app.use('/api', require("./Routes/OrderData"));
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
